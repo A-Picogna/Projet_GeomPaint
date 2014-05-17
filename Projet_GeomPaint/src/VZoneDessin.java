8cfc29313c7eed8e7f4f83c0ae55f87570814edb
Tony Vilas <tony.vilas.contact@gmail.com>
2014-05-17 10:21:22 +0200

    Test Commit Tony

--- Projet_GeomPaint/src/VZoneDessin.java
+++ Projet_GeomPaint/src/VZoneDessin.java
@@ -3,4 +3,6 @@ public class VZoneDessin {
 	// ceci est un test ? vous le voyez ? YOUHOU CA MARCHE !!!
 	
 	// Ceci est un autre test ! ici le PC fixe, m'entend tu PC portable !?!?
+	
+	// Test Commit Tony.
 }
