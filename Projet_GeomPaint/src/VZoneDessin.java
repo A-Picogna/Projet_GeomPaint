6898be33a773b849607d25e8e1913f63467cfa34
Alexandre <Alexandre@Alexandre-PC.home>
2014-05-18 12:44:22 +0200

    suppression projet, récréation d'un nouveau

--- Projet_GeomPaint/src/VZoneDessin.java
+++
@@ -1,8 +0,0 @@
-
-public class VZoneDessin {
-	// ceci est un test ? vous le voyez ? YOUHOU CA MARCHE !!!
-	
-	// Ceci est un autre test ! ici le PC fixe, m'entend tu PC portable !?!?
-	
-	// Test Commit Tony.
-}
