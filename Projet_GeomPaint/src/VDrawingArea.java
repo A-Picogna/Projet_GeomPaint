90adc43baab7c15e932c6e15741abf1953269fa2
Alexandre <Alexandre@Alexandre-PC.home>
2014-05-18 13:15:34 +0200

    ajout de quelques méthodes basique de gestion des dessins

--- Projet_GeomPaint/src/VDrawingArea.java
+++ Projet_GeomPaint/src/VDrawingArea.java
@@ -1,24 +1,26 @@
 import java.awt.Color;
 import java.util.LinkedList;
 
+import javax.swing.JPanel;
+
 
 public class VDrawingArea extends JPanel{
 	
-	private LinkedList<Model> dessin;
+	private LinkedList<Model> figureList;
 	private static int index;
 	
 	public VDrawingArea() {
-    	dessin= new LinkedList<Model>();
+    	figureList= new LinkedList<Model>();
     	index = -1;	
     	this.setBackground(Color.white);    	
     }
 
-	public LinkedList<Model> getDessin() {
-		return dessin;
+	public LinkedList<Model> getfigureList() {
+		return figureList;
 	}
 
-	public void setDessin(LinkedList<Model> dessin) {
-		this.dessin = dessin;
+	public void setfigureList(LinkedList<Model> figureList) {
+		this.figureList = figureList;
 	}
 
 	public static int getIndex() {
@@ -29,6 +31,30 @@ public class VDrawingArea extends JPanel{
 		VDrawingArea.index = index;
 	}
 
-
+	public void addFigure(Model m,Color c) {
+		figureList.add(m);
+		if(index==-1){
+			index++;
+		}else{
+			if(index>=1){
+				figureList.get(index).unSelect();
+				index++;
+			}else{
+				index++;
+			}
+		figureList.get(index).Select();
+		}
+	}
+	
+	public void deletefigure(Model mod){
+		if(index>=0)  
+			this.figureList.remove(index);
+		if(index>=1)
+			index--;
+		if(index>=0 && this.figureList.size()!=0)
+			this.figureList.get(index).select();
+		if(this.figureList.size()==0)
+			this.index=-1;				
+	}
 
 }
