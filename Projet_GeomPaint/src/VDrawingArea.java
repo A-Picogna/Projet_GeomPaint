21689dea1f682965091506675a4f868d581d0337
APicogna <alex.picogna@wanadoo.fr>
2014-05-21 17:12:56 +0200

    traitement creation figure

--- Projet_GeomPaint/src/VDrawingArea.java
+++ Projet_GeomPaint/src/VDrawingArea.java
@@ -1,24 +1,44 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.LinkedList;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class VDrawingArea extends JPanel{
+public class VDrawingArea extends JPanel implements ActionListener {
 	
-	Model drawingList;
+	Model model;
 	
 	public VDrawingArea(){
-		drawingList = new Model();
+		model = new Model();
 	}
 	
 	public void paintComponent(Graphics g) {
     	super.paintComponent(g);
-    	GeomShape figure;
+    	this.displayAll(g);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() instanceof JButton){
+			JButton j = (JButton) e.getSource();
+			if (j.getText().equals("Rectangle"));
+				this.createDrawing(new Rectangle());
+		}
+	}
+	
+	public void createDrawing(GeomShape gs){
+		int nbPoints;
+	}
+	
+	public void displayAll(Graphics g){
+		GeomShape figure;
     	int nbPoints;
-    	for(int i = 0 ; i < this.drawingList.getfigureList().size() ; i++){
-    		figure = this.drawingList.getfigureList().get(i);
+    	for(int i = 0 ; i < this.model.getfigureList().size() ; i++){
+    		figure = this.model.getFigureList().get(i);
     		nbPoints = figure.getNumberPoints();
     		for (int j = 0 ; j < nbPoints-1 ; j++){
     			g.drawLine(figure.getPointsTab[j].getX(), figure.getPointsTab[j].getY(), figure.getPointsTab[j+1].getX(), figure.getPointsTab[j+1].getY());
@@ -26,7 +46,7 @@ public class VDrawingArea extends JPanel{
     		g.drawLine(figure.getPointsTab[nbPoints-1].getX(), figure.getPointsTab[nbPoints-1].getY(), figure.getPointsTab[0].getX(), figure.getPointsTab[0].getY());
     	}
 	}
-	
+
 	
 
 }
