826890fbe96fe73434dd3c0fca576a4e59a1e98e
Tony Vilas <tony.vilas.contact@gmail.com>
2014-05-21 16:17:14 +0200

    Ajout des Getters et des Setters dans la classe GeomShape.

--- Projet_GeomPaint/src/GeomShape.java
+++ Projet_GeomPaint/src/GeomShape.java
@@ -30,7 +30,7 @@ public class GeomShape {
 	/**
 	 * Number of points of the figure.
 	 */
-	private int size;
+	private int numberPoints;
 	
 	
 ////////////////////
@@ -38,9 +38,74 @@ public class GeomShape {
 ////////////////////
 
 	
+	
 
 ///////////////////////
 //GETTERS & SETTERS //
 ///////////////////////
 
+	/**
+	 * Method getPointsTab()
+	 * @return the array of Points constituting the figure.
+	 */
+	public Point[] getPointsTab() {
+		return pointsTab;
+	}
+
+	/**
+	 * Method getColour()
+	 * @return the colour of the calling figure.
+	 */
+	public Color getColour() {
+		return colour;
+	}
+
+	/**
+	 * Method setColour(Color)
+	 * @param colour - coour to be given to the calling figure.
+	 */
+	public void setColour(Color colour) {
+		this.colour = colour;
+	}
+
+	/**
+	 * Method isSelected()
+	 * @return true if the calling figure is selected, false otherwise.
+	 */
+	public boolean isSelected() {
+		return selected;
+	}
+
+	/**
+	 * Method select()
+	 * Sets the selected attribute of the calling figure to true.
+	 */
+	public void select() {
+		this.selected = true;
+	}
+	
+	/**
+	 * Method unselect()
+	 * Sets the selected attribute of the calling figure to false.
+	 */
+	public void unselect() {
+		this.selected = false;
+	}
+
+	/**
+	 * Method getNumberPoints()
+	 * @return the number of points constituting the figure.
+	 */
+	public int getNumberPoints()
+	{
+		return this.numberPoints;
+	}
+	
+	/**
+	 * Method setNumberPoints(int)
+	 * @param numberPoints - the number of points constituting the figure.
+	 */
+	public void setNumberPoints(int numberPoints) {
+		this.numberPoints = numberPoints;
+	}
 }
