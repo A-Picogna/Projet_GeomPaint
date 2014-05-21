041c61834f966260a3e748e01417a31ebbd314b2
Alexandre <Alexandre@Alexandre-PC.home>
2014-05-21 13:15:03 +0200

    1er ajout Javadoc pour VDrawingArea

--- Projet_GeomPaint/src/VDrawingArea.java
+++ Projet_GeomPaint/src/VDrawingArea.java
@@ -7,32 +7,63 @@ import javax.swing.JPanel;
 
 public class VDrawingArea extends JPanel{
 	
+	/**
+	 * Cette classe n'a vocation qu'a afficher les figures
+	 * suivant les zones cliqué et le bouton ou mode selectionné
+	 * on utilise une liste de figure issue de la classe Model
+	 * et d'un indice statique qui représente virtuellement la figure en cours
+	 * de selection
+	 */	
 	private LinkedList<Model> figureList;
 	private static int index;
 	
+	/**
+	 * Constructeur qui cré une liste de Modele et définie le fond en blanc
+	 */	
 	public VDrawingArea() {
-    	figureList= new LinkedList<Model>();
-    	index = -1;	
+    	figureList = new LinkedList<Model>();
     	this.setBackground(Color.white);    	
     }
 
+	/**
+	 * Getter - retourne la liste de figures
+	 * @return
+	 */	
 	public LinkedList<Model> getfigureList() {
 		return figureList;
 	}
 
+	/**
+	 * Setter - modifie la liste de figure
+	 * @param figureList
+	 */
 	public void setfigureList(LinkedList<Model> figureList) {
 		this.figureList = figureList;
 	}
 
+	/**
+	 * Getter - retourne l'indice actuel
+	 * @return
+	 */	
 	public static int getIndex() {
 		return index;
 	}
 
+	/**
+	 * Setter - modifie l'indice
+	 * @return
+	 */	
 	public static void setIndex(int index) {
 		VDrawingArea.index = index;
 	}
 
-	public void addFigure(Model m,Color c) {
+	/**
+	 * fonction qui permet d'ajouter une figure en fin de liste
+	 * et on incrémente l'indice après avoir déselectionné la figure en cours
+	 * et selectionné la figure qui vient d'être créer
+	 * @param m
+	 */
+	public void addFigure(Model m) {
 		figureList.add(m);
 		if(index==-1){
 			index++;
@@ -47,6 +78,11 @@ public class VDrawingArea extends JPanel{
 		}
 	}
 	
+	/**
+	 * on retire la figure de la liste correspondant à la valeur de l'indice
+	 * ce qui revient a supprimer la figure
+	 * @param mod
+	 */
 	public void deletefigure(Model mod){
 		if(index>=0)  
 			this.figureList.remove(index);
@@ -58,6 +94,11 @@ public class VDrawingArea extends JPanel{
 			this.index=-1;				
 	}
 	
+	/**
+	 * récupere la figure (l'objet) présent a la position
+	 * donné par l'indice
+	 * @return
+	 */
 	public Model getFigureSelection(){
 		Model res;
 		if(index>=0 && this.figureList.size()!=0)
@@ -67,6 +108,11 @@ public class VDrawingArea extends JPanel{
 		return res;
 	}
 	
+	/**
+	 * on redessine toute la liste de figure
+	 * ce qui revient a tracé des lignes entre les points
+	 * de chaque figure
+	 */
 	public void paintComponent(Graphics g) {
     	super.paintComponent(g);
     	for(int i = 0; i< this.figureList.size() ; i++)
