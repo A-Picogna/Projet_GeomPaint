2e3815ac8d7c8d42593ac687c6de3fac41a9120b
Tony Vilas <tony.vilas.contact@gmail.com>
2014-05-22 18:36:11 +0200

    Création de la méthode Polygon.contains(Point).

--- Projet_GeomPaint/src/Polygon.java
+++ Projet_GeomPaint/src/Polygon.java
@@ -8,6 +8,25 @@
 public class Polygon extends GeomShape {
 	
 	
-	
+
+    /**
+     * Return true if the given point is contained inside the boundary.
+     * @param test The point to check
+     * @return true if the point is inside the boundary, false otherwise
+     *
+     */
+    public boolean contains(Point test) {
+    	int i, j;
+    	
+    	Point[] points = this.getPointsTab();
+    	boolean result = false;
+    	for (i = 0, j = this.getNumberPoints() - 1; i < this.getNumberPoints(); j = i++) {
+    		if ((points[i].getY() > test.getY()) != (points[j].getY() > test.getY()) &&
+    				(test.getX() < (points[j].getX() - points[i].getX()) * (test.getY() - points[i].getY()) / (points[j].getY()-points[i].getY()) + points[i].getX())) {
+    			result = !result;
+    		}
+    	}
+      return result;
+    }
 
 }
