554fd9ddbcda3daa85a606027e5f0b0356339519
Tony Vilas <tony.vilas.contact@gmail.com>
2014-05-22 18:28:20 +0200

    Début de création de la classe cercle. Ajout du constructeur

---
+++ Projet_GeomPaint/src/Circle.java
@@ -0,0 +1,19 @@
+/**
+ * Class Circle
+ * Describes a Circle.
+ * A Circle is a sort of GeomShape.
+ * @author VILAS
+ *
+ */
+public class Circle extends GeomShape{
+		
+	
+	public Circle (Point[]data)
+	{
+		this.setNumberPoints(2);
+		this.setPointsTab(data);
+	}
+	
+	
+
+}
