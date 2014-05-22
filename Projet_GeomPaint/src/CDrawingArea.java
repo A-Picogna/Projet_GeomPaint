db63f9957e2a282cbcf8c4ba8aa617030f0ba2cb
Xavi <Xavi@192.168.167.127>
2014-05-22 16:39:28 +0200

    CDrawingArea

--- Projet_GeomPaint/src/CDrawingArea.java
+++ Projet_GeomPaint/src/CDrawingArea.java
@@ -10,8 +10,9 @@ public class CDrawingArea implements MouseListener, MouseMotionListener {
 	
 	//ATTRIBUTES
 	private Model model;
-	private int x, lastx, y, lasty, point;
+	private int x,  y, point;
 	private Point p;
+	
 
 	
 	public void mouseDragged(MouseEvent e) {
@@ -37,8 +38,7 @@ public class CDrawingArea implements MouseListener, MouseMotionListener {
 			    		}
 				
 				}
-			lastx = e.getX();
-			lasty = e.getY();
+			
 			}
 		}
 	}
@@ -51,6 +51,13 @@ public class CDrawingArea implements MouseListener, MouseMotionListener {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		//recuperation points dessin de figure
+		
+		
+		
+		
+		
+		//selection de figure
 		if(SwingUtilities.isLeftMouseButton(e) && model.getFigureList().size() > 0 ){
 			x = e.getX();
 			y = e.getY();
@@ -59,6 +66,7 @@ public class CDrawingArea implements MouseListener, MouseMotionListener {
 			for (int i =0; i<this.model.getFigureList().size()-1; i++){
 				//si la figure sur laquel on clic n'est pas selectionée on change son état
 				if(this.model.getFigureList().get(i).contains(p) && !this.model.getFigureList().get(i).isSelected()){
+					this.model.getSelected.unselect();
 					this.model.getFigureList().get(i).select();
 				}
 			}
