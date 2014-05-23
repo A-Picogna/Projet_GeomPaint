ae536d25881d0a950aa8e87d7c421f73a1d69b80
Tony Vilas <tony.vilas.contact@gmail.com>
2014-05-23 15:12:54 +0200

    Modification du Main ajout de lien observer / observable.

--- Projet_GeomPaint/src/Main.java
+++ Projet_GeomPaint/src/Main.java
@@ -1,24 +1,35 @@
 import java.awt.*;
+
 import javax.swing.*;
+
 import java.awt.event.*;
 
 
 public class Main {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("GeomPaint");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Definition of the Frame
+		JFrame window = new JFrame("GeomPaint");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Generation of the different elements constituting the application :
+			// Model.
 		Model m = new Model();
+			// View for the drawing part of the model.
 		VDrawingArea vda = new VDrawingArea(m);
-		CDrawingArea cda = new CDrawingArea();
+			// View for the ToolBar part of the model.
 		ToolBar tb = new ToolBar(m);
+			// View assembling together the VDrawingArea and the ToolBar views.
 		Organiser paintSoftware = new Organiser(vda,tb);
-		vda.addMouseListener(cda);
-		vda.addMouseMotionListener(cda);
+		
+		// Implementation of the Model's Observer.
+		m.addObserver(vda);
+		
+		// Definition of the size and specifications for the window.
 		paintSoftware.setPreferredSize(new Dimension(600,600));
-		frame.setContentPane(paintSoftware);
-		frame.pack();
-		frame.setVisible(true);
-		frame.setResizable(false);
+		window.setContentPane(paintSoftware);
+		window.pack();
+		window.setVisible(true);
+		window.setResizable(false);
 		paintSoftware.setFocusable(true);		  
 	}
 }
