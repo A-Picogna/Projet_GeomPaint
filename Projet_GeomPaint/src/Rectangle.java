7b8d13dadbe2b8962769def64a50fd7865bd6ad5
Tony Vilas <tony.vilas.contact@gmail.com>
2014-05-23 09:02:58 +0200

    Creation de la classe rectangle. Ajout d'un constructeur Rectangle(Point, Point).

---
+++ Projet_GeomPaint/src/Rectangle.java
@@ -0,0 +1,31 @@
+/**
+ * Class Rectangle,
+ * describes a rectangular figure.
+ * A Rectangle is a kind of Polygon.
+ * @author VILAS
+ *
+ */
+public class Rectangle extends Polygon{
+	
+	/**
+	 * Constructor of the class Polygon
+	 * admitting 2 points as arguments.
+	 * @param a - point constituting the top left corner of the Rectangle.
+	 * @param c - point constituting the bottom right corner of the Rectangle.
+	 */
+	public Rectangle(Point a, Point c)
+	{
+		super();
+		int xa = a.getX();
+		int ya = a.getY();
+		int xc = c.getX();
+		int yc = c.getY();
+		// Creation of an array of points corresponding to the coordinates of the rectangle's angles
+		// from the top left corner to the bottom left corner, clockwise.
+		Point[]data= {new Point(xa,ya), new Point(xc,yc), new Point(xc,yc), new Point(xa,yc)};
+		
+		this.setNumberPoints(data.length);
+		this.setPointsTab(data);
+	}
+
+}
