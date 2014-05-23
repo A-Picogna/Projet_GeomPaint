f79520fa51c2d2e7f69d81595994e662bd4aed13
Tony Vilas <tony.vilas.contact@gmail.com>
2014-05-23 17:41:40 +0200

    Modifications in the class ToolBar : - Stopped implementing ActionListener. - Defined a specific ActionListener for each Component.

--- Projet_GeomPaint/src/ToolBar.java
+++ Projet_GeomPaint/src/ToolBar.java
@@ -1,9 +1,10 @@
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
 
 
-public class ToolBar extends JToolBar implements ActionListener {
+public class ToolBar extends JToolBar {
 	private Model model;
 	// Elements constituting the Circle Button
 	private final ImageIcon circleIcon = createImageIcon("icons/circle.gif");
@@ -51,124 +52,138 @@ public class ToolBar extends JToolBar implements ActionListener {
         colorlist.setSelectedIndex(0);
 		this.add(colorlist);
 		
-		circle.addActionListener(this);
-		rectangle.addActionListener(this);
-		triangle.addActionListener(this);
-		polygon.addActionListener(this);
-		fill.addActionListener(this);
-		duplicate.addActionListener(this);
-		erase.addActionListener(this);
-		colorlist.addActionListener(this);
-	}
-    public void actionPerformed(ActionEvent e) {
-		if ((JButton)e.getSource() == circle) {
-			model.setMode('c');
-			Model.setNbPointsRequired(2);
-		}
-		if ((JButton)e.getSource() == rectangle) {
-			model.setMode('r');
-			Model.setNbPointsRequired(2);
-		}
-		if ((JButton)e.getSource() == triangle) {
-			model.setMode('t');
-			Model.setNbPointsRequired(3);
-		}
-/*		if ((JButton)e.getSource() == polygon) {
+		circle.addActionListener(new ActionListener(){
 
-			String s = JOptionPane.showInputDialog(
-                this.getRootPane(),
-                "Choisissez le nombre d'angles \n"+
-                "de votre polygone:\n",
-                "Choix de n",
-                JOptionPane.PLAIN_MESSAGE);
-			try {
-				int n = Integer.parseInt(s);
-				if (n<3)
+			public void actionPerformed(ActionEvent e) {
+				model.setMode('c');
+				Model.setNbPointsRequired(2);
+			}
+			
+		});
+		rectangle.addActionListener(new ActionListener(){
+			
+				public void actionPerformed(ActionEvent e) {
+				model.setMode('r');
+				Model.setNbPointsRequired(2);
+			}
+		});
+		triangle.addActionListener(new ActionListener(){
+			
+				public void actionPerformed(ActionEvent e) {
+				model.setMode('t');
+				Model.setNbPointsRequired(3);
+			}
+		});
+		/* polygon.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e) {
+				String s = JOptionPane.showInputDialog(
+	                this.getRootPane(),
+	                "Choisissez le nombre d'angles \n"+
+	                "de votre polygone:\n",
+	                "Choix de n",
+	                JOptionPane.PLAIN_MESSAGE);
+				try {
+					int n = Integer.parseInt(s);
+					if (n<3)
+						showWarning("Le nombre d'angles \n"+
+									"du polygone\n"+
+									"doit etre superieur a 2\n");
+					else {
+						model.setMode('p');
+						Model.setNbPointsRequired(n);
+						return;
+					}
+				}
+				catch (NumberFormatException nfe) {
 					showWarning("Le nombre d'angles \n"+
 								"du polygone\n"+
-								"doit etre superieur a 2\n");
-				else {
-					model.setMode('p');
-					Model.setNbPointsRequired(n);
-					return;
+								"doit etre un entier\n");
 				}
 			}
-			catch (NumberFormatException nfe) {
-				showWarning("Le nombre d'angles \n"+
-							"du polygone\n"+
-							"doit etre un entier\n");
-			}
-		}*/
-		if ((JButton)e.getSource() == fill) {
-			GeomShape g = model.getSelected();
-			if (g != null) {
-				if (g.isFilled())
-					g.setFilled(false);
-				else
-					g.setFilled(true);
-			}
-		}
-		if ((JButton)e.getSource() == duplicate) {
-			GeomShape g = model.getSelected();
-			if (g != null) {
-				if (g instanceof Circle) {
-						Circle c = new Circle((Circle)g);
-						model.addFigure(c);
-						c.translate(10,10);
-				}
-				if (g instanceof Rectangle) {
-						Rectangle r = new Rectangle((Rectangle)g);
-						model.addFigure(r);
-						r.translate(10,10);
-				}
-				if (g instanceof Triangle) {
-						Triangle t = new Triangle((Triangle)g);
-						model.addFigure(t);
-						t.translate(10,10);
-				}
-				if (g instanceof Polygon) {
-						Polygon p = new Polygon((Polygon)g);
-						model.addFigure(p);
-						p.translate(10,10);
+		}); */
+		fill.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e) {
+				GeomShape g = model.getSelected();
+				if (g != null) {
+					if (g.isFilled())
+						g.setFilled(false);
+					else
+						g.setFilled(true);
 				}
 			}
-		}
-		if ((JButton)e.getSource() == erase) {
-			GeomShape g = model.getSelected();
-			if (g != null) {
-				int i = model.getFigureList().indexOf(g);
-				model.delFigure(i);
-			}
-		}
-		if ((JComboBox)e.getSource() == colorlist) {
-			GeomShape g = model.getSelected();
-			if (g != null) {
-				String s = (String)colorlist.getSelectedItem();
-				switch (s) {	
-					case "Rouge":
-						g.setColour(Color.black);
-						break;
-					case "Bleu":
-						g.setColour(Color.blue);
-						break;
-					case "Jaune":
-						g.setColour(Color.yellow);
-						break;
-					case "Vert":
-						g.setColour(Color.green);
-						break;
-					case "Orange":
-						g.setColour(Color.orange);
-						break;
-					case "Rose":
-						g.setColour(Color.pink);
-						break;
-					default:
-						g.setColour(Color.black);
+		});
+		duplicate.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e) {
+				GeomShape g = model.getSelected();
+				if (g != null) {
+					if (g instanceof Circle) {
+							Circle c = new Circle((Circle)g);
+							model.addFigure(c);
+							c.translate(10,10);
+					}
+					if (g instanceof Rectangle) {
+							Rectangle r = new Rectangle((Rectangle)g);
+							model.addFigure(r);
+							r.translate(10,10);
+					}
+					if (g instanceof Triangle) {
+							Triangle t = new Triangle((Triangle)g);
+							model.addFigure(t);
+							t.translate(10,10);
+					}
+					if (g instanceof Polygon) {
+							Polygon p = new Polygon((Polygon)g);
+							model.addFigure(p);
+							p.translate(10,10);
+					}
 				}
 			}
-		}
-    }
+		});
+		erase.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e) {
+				GeomShape g = model.getSelected();
+				if (g != null) {
+					int i = model.getFigureList().indexOf(g);
+					model.delFigure(i);
+				}
+			}
+		});
+		colorlist.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e) {
+				GeomShape g = model.getSelected();
+				if (g != null) {
+					String s = (String)colorlist.getSelectedItem();
+					switch (s) {	
+						case "Rouge":
+							g.setColour(Color.black);
+							break;
+						case "Bleu":
+							g.setColour(Color.blue);
+							break;
+						case "Jaune":
+							g.setColour(Color.yellow);
+							break;
+						case "Vert":
+							g.setColour(Color.green);
+							break;
+						case "Orange":
+							g.setColour(Color.orange);
+							break;
+						case "Rose":
+							g.setColour(Color.pink);
+							break;
+						default:
+							g.setColour(Color.black);
+					}
+				}
+			}
+		});
+	}
 
 	
 	public void showWarning(String s) {
